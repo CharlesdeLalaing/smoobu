150f62ec8e2356f3bb0effb58c1b5fafc95f4959
MeHigh22 <72247500+MeHigh22@users.noreply.github.com>
2024-12-18 15:20:35 +0100

    PushBaseIntialisation

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BookingConfirmation from "./components/BookingConfirmation";
-import Booking2 from "./components/booking/BookingForm"
-import './index.css'
+import Booking2 from "./components/booking/BookingForm";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n";
+import "./index.css";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/booking-confirmation" element={<BookingConfirmation />} />
-        <Route path="/" element={<Booking2 />} />
-      </Routes>
-    </Router>
+    <I18nextProvider i18n={i18n}>
+      <Router>
+        <Routes>
+          <Route
+            path="/booking-confirmation"
+            element={<BookingConfirmation />}
+          />
+          <Route path="/" element={<Booking2 />} />
+        </Routes>
+      </Router>
+    </I18nextProvider>
   );
 }
 
