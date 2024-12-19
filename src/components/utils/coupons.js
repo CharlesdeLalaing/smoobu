64b75111e56cfa8ba7f29c70ef5450423151ca6e
charles de Lalaing <71701904+CharlesdeLalaing@users.noreply.github.com>
2024-12-19 11:52:13 +0100

    coupons added

--- src/components/utils/coupons.js
+++ src/components/utils/coupons.js
@@ -1,8 +1,152 @@
 export const VALID_COUPONS = {
   TESTDISCOUNT: {
     discount: 10,
-    type: "fixed", // 'fixed' for euro amount, 'percentage' for percentage discount
+    type: "fixed",
     currency: "EUR",
   },
-  // Add more coupons as needed
+  DOME390: {
+    discount: 390,
+    type: "fixed",
+    currency: "EUR",
+  },
+  DOME215: {
+    discount: 215,
+    type: "fixed",
+    currency: "EUR",
+  },
+  BULLE305: {
+    discount: 305,
+    type: "fixed",
+    currency: "EUR",
+  },
+  DOME235: {
+    discount: 235,
+    type: "fixed",
+    currency: "EUR",
+  },
+  DOME295: {
+    discount: 295,
+    type: "fixed",
+    currency: "EUR",
+  },
+  BULLE400: {
+    discount: 400,
+    type: "fixed",
+    currency: "EUR",
+  },
+  "GIFT.6104": {
+    discount: 150,
+    type: "fixed",
+    currency: "EUR",
+  },
+  "GIFT.6096": {
+    discount: 300,
+    type: "fixed",
+    currency: "EUR",
+  },
+  "GIFT.6105": {
+    discount: 200,
+    type: "fixed",
+    currency: "EUR",
+  },
+  "GIFT.6111": {
+    discount: 180,
+    type: "fixed",
+    currency: "EUR",
+  },
+  "GIFT.6112": {
+    discount: 360,
+    type: "fixed",
+    currency: "EUR",
+  },
+  "GIFT.6496": {
+    discount: 190,
+    type: "fixed",
+    currency: "EUR",
+  },
+  "GIFT.6714": {
+    discount: 275,
+    type: "fixed",
+    currency: "EUR",
+  },
+  "GIFT.6898": {
+    discount: 265,
+    type: "fixed",
+    currency: "EUR",
+  },
+  "GIFT.6899": {
+    discount: 315,
+    type: "fixed",
+    currency: "EUR",
+  },
+  "GIFT.6901": {
+    discount: 200,
+    type: "fixed",
+    currency: "EUR",
+  },
+  "GIFT.6968": {
+    discount: 400,
+    type: "fixed",
+    currency: "EUR",
+  },
+  "GIFT.6969": {
+    discount: 315,
+    type: "fixed",
+    currency: "EUR",
+  },
+  "GIFT.6970": {
+    discount: 235,
+    type: "fixed",
+    currency: "EUR",
+  },
+  "GIFT.6973": {
+    discount: 210,
+    type: "fixed",
+    currency: "EUR",
+  },
+  "GIFT.7082": {
+    discount: 360,
+    type: "fixed",
+    currency: "EUR",
+  },
+  "GIFT.7095": {
+    discount: 270,
+    type: "fixed",
+    currency: "EUR",
+  },
+  "GIFT.7105": {
+    discount: 215,
+    type: "fixed",
+    currency: "EUR",
+  },
+  "GIFT.7157": {
+    discount: 240,
+    type: "fixed",
+    currency: "EUR",
+  },
+  "GIFT.10476": {
+    discount: 315,
+    type: "fixed",
+    currency: "EUR",
+  },
+  "GIFT.10492": {
+    discount: 245,
+    type: "fixed",
+    currency: "EUR",
+  },
+  "GIFT.10495": {
+    discount: 210,
+    type: "fixed",
+    currency: "EUR",
+  },
+  "GIFT.10551": {
+    discount: 275,
+    type: "fixed",
+    currency: "EUR",
+  },
+  "GIFT.10577": {
+    discount: 230,
+    type: "fixed",
+    currency: "EUR",
+  }
 };
