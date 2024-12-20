b7eb7de19669a02536376e48ad3c9391f31172b8
charles de Lalaing <71701904+CharlesdeLalaing@users.noreply.github.com>
2024-12-20 15:16:36 +0100

    better stripe

--- src/components/PaymentForm.jsx
+++ src/components/PaymentForm.jsx
@@ -7,7 +7,7 @@ const PaymentForm = ({ onSuccess, onError }) => {
   const { t } = useTranslation();
   
 
-const stripe = us!é&@¥#£QAeStripe();
+const stripe = useStripe();
   const elements = useElements();
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState(null);
