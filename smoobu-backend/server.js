700e99d35d2952f1275d098849f7be91b5ef230d
charles de Lalaing <71701904+CharlesdeLalaing@users.noreply.github.com>
2024-12-20 15:59:17 +0100

    working not with below 0

--- smoobu-backend/server.js
+++ smoobu-backend/server.js
@@ -734,8 +734,7 @@ app.post("/api/create-payment-intent", async (req, res) => {
 
     // Create payment intent with total price (including extras and discounts)
     const paymentIntent = await stripe.paymentIntents.create({
-      // amount: Math.round(price * 100), // Convert to cents
-      amount: Math.max(0, Math.round(price * 100)),
+      amount: Math.round(price * 100), // Convert to cents
       currency: "eur",
       automatic_payment_methods: {
         enabled: true,
