6a02b66f6bed318b83985015d1ff68b4a869192d
charles de Lalaing <71701904+CharlesdeLalaing@users.noreply.github.com>
2024-12-20 17:26:17 +0100

    next public

--- src/components/StripeWrapper.jsx
+++ src/components/StripeWrapper.jsx
@@ -3,7 +3,7 @@ import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 
 const stripePromise = loadStripe(
-  process.env.STRIPE_PK_KEY
+  process.env.NEXT_PUBLIC_STRIPE_PK_KEY
 );
 
 const StripeWrapper = ({ clientSecret, children, onSuccess, onError }) => {
