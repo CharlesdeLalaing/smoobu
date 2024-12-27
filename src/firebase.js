cf4feb350c1e50b858c4f6ea88a0d5a0e3f8dab8
charles de Lalaing <71701904+CharlesdeLalaing@users.noreply.github.com>
2024-12-27 15:29:24 +0100

    test with files

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyC3b4kut2R0hnK3Nwdsnd271Uy-cEcAuKo",
+  authDomain: "ferme-de-basseilles-9c7ff.firebaseapp.com",
+  projectId: "ferme-de-basseilles-9c7ff",
+  storageBucket: "ferme-de-basseilles-9c7ff.firebasestorage.app",
+  messagingSenderId: "159647254828",
+  appId: "1:159647254828:web:7b461d976e3e3fb5914489",
+  measurementId: "G-EKS75CPLKR"
 };
 
 // Initialize Firebase
