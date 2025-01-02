aeb7ff8d9ee82053b15aa77da065a4414c909f5d
charles de Lalaing <71701904+CharlesdeLalaing@users.noreply.github.com>
2025-01-02 10:54:08 +0100

    now

--- src/firebase.js
+++ src/firebase.js
@@ -14,8 +14,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db_2 = getFirestore(app);
+const db2 = getFirestore(app);
 const auth = getAuth(app);
 
-export { db_2, auth };
+export { db2, auth };
 
