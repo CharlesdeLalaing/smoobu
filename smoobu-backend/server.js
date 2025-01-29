8eae9adefd5ed1252f058d10d456e09443f796e2
MeHigh22 <72247500+MeHigh22@users.noreply.github.com>
2025-01-29 19:28:05 +0100

    Translations

--- smoobu-backend/server.js
+++ smoobu-backend/server.js
@@ -353,7 +353,7 @@ app.post(
       event = stripe.webhooks.constructEvent(
         req.body,
         sig,
-        "whsec_sbVaa5obD8UFZ5hzg0iEZpLHozrHi4Z8"
+        "whsec_d9b86273072de6b319134fbc08752e2b4e66bae72aaa2cf4cb7db1411974c20a"
       );
 
       if (event.type === "payment_intent.succeeded") {
