bf50b974ed0778116195a30000f5315ebdf0fdb7
charles de Lalaing <71701904+CharlesdeLalaing@users.noreply.github.com>
2025-02-14 09:58:39 +0100

    added 0 children

--- src/components/booking/SearchSection.jsx
+++ src/components/booking/SearchSection.jsx
@@ -204,7 +204,7 @@ export const SearchSection = ({
                 </Listbox.Button>
 
                 <Listbox.Options className="absolute z-10 w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-56 ring-1 ring-black/5 focus:outline-none sm:text-sm">
-                  {[1, 2, 3, 4, 5, 6, 7, 8].map((num) => (
+                  {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((num) => (
                     <Listbox.Option
                       key={num}
                       value={num}
