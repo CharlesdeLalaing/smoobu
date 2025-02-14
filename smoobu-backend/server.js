7fa9208d32e5e8ac550410cb08223e9283cb8811
charles de Lalaing <71701904+CharlesdeLalaing@users.noreply.github.com>
2025-02-14 15:46:49 +0100

    now

--- smoobu-backend/server.js
+++ smoobu-backend/server.js
@@ -1213,9 +1213,8 @@ app.get("/api/bookings-report", async (req, res) => {
         params: {
           arrivalFrom: startDate,
           arrivalTo: endDate,
-          excludeBlocked: false,
+          excludeBlocked: true,
           showCancellation: true,
-          pageSize: 100 
         },
       }
     );
