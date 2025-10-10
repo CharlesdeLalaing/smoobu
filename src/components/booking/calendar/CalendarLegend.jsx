43662f1260a8ecc95d7a2b8a858ac81ee3596f62
Mihai <pmihai31@gmail.com>
2025-10-10 11:40:56 +0200

    CalendarLegendAdded

--- src/components/booking/calendar/CalendarLegend.jsx
+++ src/components/booking/calendar/CalendarLegend.jsx
@@ -3,22 +3,34 @@ import PropTypes from "prop-types";
 
 export const CalendarLegend = ({ t }) => {
   return (
-    <div className="calendar-legend">
-      <div className="legend-item">
-        <div className="legend-color available"></div>
-        <span>{t("calendar.available")}</span>
+    <div>
+      <div className="calendar-legend">
+        <div className="legend-item">
+          <div className="legend-color available"></div>
+          <span>{t("calendar.available")}</span>
+        </div>
+        <div className="legend-item">
+          <div className="legend-color unavailable"></div>
+          <span>{t("calendar.unavailable")}</span>
+        </div>
+        <div className="legend-item">
+          <div className="legend-color partially-available-checkIn"></div>
+          <span>{t("calendar.partiallyAvailableCheckIn")}</span>
+        </div>
+        <div className="legend-item">
+          <div className="legend-color partially-available"></div>
+          <span>{t("calendar.partiallyAvailableCheckOut")}</span>
+        </div>
       </div>
-      <div className="legend-item">
-        <div className="legend-color unavailable"></div>
-        <span>{t("calendar.unavailable")}</span>
-      </div>
-      <div className="legend-item">
-        <div className="legend-color partially-available-checkIn"></div>
-        <span>{t("calendar.partiallyAvailableCheckIn")}</span>
-      </div>
-      <div className="legend-item">
-        <div className="legend-color partially-available"></div>
-        <span>{t("calendar.partiallyAvailableCheckOut")}</span>
+      <div className="calendar-times">
+        <div className="time-item">
+          <div className="time-dot checkin-dot"></div>
+          <span>{t("calendar.checkInTime")}</span>
+        </div>
+        <div className="time-item">
+          <div className="time-dot checkout-dot"></div>
+          <span>{t("calendar.checkOutTime")}</span>
+        </div>
       </div>
     </div>
   );
